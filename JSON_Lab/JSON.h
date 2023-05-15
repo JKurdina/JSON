22f05af7da4ee67a441a978b2fc8d9a1fe5b6874
Julia Kurdina <kurdina.00@gmail.com>
2023-05-15 19:54:03 +0300

    finish

--- JSON_Lab/JSON.h
+++ JSON_Lab/JSON.h
@@ -3,6 +3,9 @@
 #include <stack>
 
 #include <msclr\marshal_cppstd.h>
+#include <sstream>
+#include <fstream>
+using namespace System;
 
 
 class JSon {
@@ -24,10 +27,10 @@ public:
   ListValue* get_root();
   ListValue* get_current();
   ListValue* find_key(ListValue* curr, string key, ListValue* find);
-  void new_value(string key, string value);
-  void delete_obj(string key);
+  void new_value(string value);
+  void delete_obj(string key, string value);
   void delete_obj_list(ListValue* curr);
-  void new_obj(string key_up, string key, string value);
+  void new_obj(string key, string value);
 };
 
 
@@ -42,11 +45,81 @@ namespace JSonLib
     }
 
     void load(System::String^ filename) {
-      //   js->load(filename);
       string str_fn = msclr::interop::marshal_as<std::string>(filename);
       js->load(str_fn);
+    }
+
+    void save(System::String^ filename)
+    {
+      string str_fn = msclr::interop::marshal_as<std::string>(filename);
+      js->save(str_fn);
+    }
+
+    System::String^ get_key_value() {
+      string str;
+      if (js->current->list.size() != 0)
+      {str = "'" + js->current->get_key() + "'" + ":";}
+      else { str = "'" + js->current->get_key() + "'" + ":" + "'" + js->current->get_Val() + "'"; }
+      String^ str_fn = msclr::interop::marshal_as<System::String^>(str);
+
+      String^ new_str = gcnew String(str_fn);
+      MarshalString(new_str, str);
+
+      
+      return new_str;
+    }
+
+
+
+    void MarshalString(String^ s, string& os) {
+      using namespace Runtime::InteropServices;
+      const char* chars =
+        (const char*)(Marshal::StringToHGlobalAnsi(s)).ToPointer();
+      os = chars;
+      Marshal::FreeHGlobal(IntPtr((void*)chars));
+    }
+
+    void down()
+    {
+      js->down();
 
     }
+
+    void next()
+    {
+      js->next();
+
+    }
+
+    void back()
+    {
+      js->back();
+    }
+
+    void prev()
+    {
+      js->prev();
+    }
+
+    void new_value(System::String^ str)
+    {
+      string str_fn = msclr::interop::marshal_as<std::string>(str);
+      js->new_value(str_fn);
+    }
+
+    void new_obj(System::String^ key, System::String^ value)
+    {
+      string str_key = msclr::interop::marshal_as<std::string>(key);
+      string str_value = msclr::interop::marshal_as<std::string>(value);
+      js->new_obj(str_key, str_value);
+    }
+
+    void delete_obj(System::String^ key, System::String^ value)
+    {
+      string str_key = msclr::interop::marshal_as<std::string>(key);
+      string str_value = msclr::interop::marshal_as<std::string>(value);
+      js->delete_obj(str_key, str_value);
+    }
   };
-  //מאמאןנ
+ 
 }
